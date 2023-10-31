090ee6ac74bd0db263dc149b2c0c874284951e48
anniegmatic <anna_julia_wojcik@wp.pl>
2023-10-31 14:07:00 +0100

    Pierwszy Commit

---
+++ Assets/Scripts/ToolsCharacterController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ToolsCharacterController : MonoBehaviour
+{
+    CharacterController2D character;
+    Rigidbody2D rgbd2d;
+    [SerializeField] float offsetDistance = 1f;
+    [SerializeField] float sizeOfInteractableArea = 1.2f;
+    [SerializeField] MarkerManager markerManager;
+    [SerializeField] TileMapReadController tileMapReadController;
+    [SerializeField] float maxDistance = 1.5f;
+
+    Vector3Int selectedTilePosition;
+    bool selectable;
+
+    private void Awake()
+    {
+        character = GetComponent<CharacterController2D>();
+        rgbd2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        SelectTile();
+        CanSelectCheck();
+        Marker();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            UseTool();
+        }
+    }
+
+    private void SelectTile()
+    {
+        selectedTilePosition = tileMapReadController.GetGridPosition(Input.mousePosition, true);
+    }
+
+    void CanSelectCheck()
+    {
+        Vector2 characterPosition = transform.position;
+        Vector2 cameraPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        selectable = Vector2.Distance(characterPosition, cameraPosition) < maxDistance;
+        markerManager.Show(selectable);
+    }
+
+    private void Marker()
+    {
+        markerManager.markedCellPosition = selectedTilePosition;
+    }
+
+    private void UseTool()
+    {
+        Vector2 position = rgbd2d.position + character.lastMotionVector * offsetDistance;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(position, sizeOfInteractableArea);
+
+        foreach(Collider2D c in colliders)
+        {
+            ToolHit hit = c.GetComponent<ToolHit>();
+            if (hit != null)
+            {
+                hit.Hit();
+                break;
+            }
+        }
+    }
+}
+
